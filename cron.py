698030880a90cb280e30bc979b9411766e9ba7cb
Adithya Balaji <adithyabsk@gmail.com>
2021-02-17 01:21:28 -0500

    Prepare project for Heroku.

--- cron.py
+++ cron.py
@@ -9,7 +9,7 @@ from bot import main, sponsor_tweet
 # TODO: figure out handling custom timezones
 # Create an instance of scheduler and add function
 scheduler = BlockingScheduler(timezone=get_localzone())
-scheduler.add_job(main, "cron", minute=5, hour=17)  # 5:05pm server time
-scheduler.add_job(sponsor_tweet, minute=5, hour=17, day=4)  # 5:05pm once a month
+scheduler.add_job(main, "cron", minute=5)  # , hour=17)  # 5:05pm server time
+scheduler.add_job(sponsor_tweet, "cron", minute=5)  # , hour=17, day=4)  # 5:05pm once a month
 
 scheduler.start()
